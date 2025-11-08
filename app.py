62a169da147bc39ea0ea9918bfb3a09c9c57fd1d
richard.nguyen1 <richard.nguyen2005@outlook.com>
2025-11-08 14:04:45 -0700

    Made everything in the same directory

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template
 from markupsafe import escape
-import mysql.connector
 
 app = Flask(__name__) 
 
 @app.route('/about')
 def about():
-        #return 'This is the about page and what we do'
-        return render_template('about.html')
+    #return 'This is the about page and what we do'
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    #return 'This is the about page and what we do'
+    return render_template('test.html')
 
 if __name__ == '__main__':
         print("Working")
