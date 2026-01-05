3d8d07343e89a0c5521af97f75883324d4ba4111
Rui <linusyrc@gmail.com>
2026-01-05 13:57:07 -0700

    initial imports for changing this to FastAPI

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, jsonify, Response
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+
 import requests
 import math
 from functools import lru_cache
@@ -6,8 +11,11 @@ import time
 import json
 import os
 
-app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024 
+# app = Flask(__name__)
+# app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+app = FastAPI()
+MAX_CONTENT_LENGTH = 50 * 1024 * 1024 
+templates = Jinja2Templates(directory="templates")
 
 
 # Helpers for spherical math and geometry rep extraction
@@ -16,11 +24,12 @@ def _latlon_to_unit_vec(lat, lon):
 
     Uses the same convention as client latLonToVector3 (right-handed mapping).
     """
-    phi = math.radians(90.0 - float(lat))
-    theta = math.radians(float(lon) + 180.0)
-    x = -(math.sin(phi) * math.cos(theta))
-    z = math.sin(phi) * math.sin(theta)
-    y = math.cos(phi)
+    lat = math.radians(lat)
+    lon = math.radians(lon)
+
+    x = math.cos(lat) * math.cos(lon)
+    y = math.sin(lat)
+    z = math.cos(lat) * math.sin(lon)
     return (x, y, z)
 
 
