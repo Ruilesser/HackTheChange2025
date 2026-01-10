b5e30c629e18c24ea78adb974a4bb7588e53b7a5
Rui <linusyrc@gmail.com>
2026-01-10 12:42:56 -0700

    main is up - you must do https://localhost:8000/redoc to see it

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,11 +10,24 @@ import time
 import json
 import os
 
-app = FastAPI()
+# Documentation for this I believe
+app = FastAPI(
+    title = "PlanetPulse Map API",
+    description = "API designed to gather information and produce results based on location data",
+    version = "0.1.0",
+    docs_url = "/docs",
+    redoc_url = "/redoc",
+)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], # GET, POST, PUT
+    allow_headers=["*"], # additional information to send
 )
+
+# if designed so that we run the import if we run the main python file directly
+if __name__ == "__main__":
+        import uvicorn # webserver
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
